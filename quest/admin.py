8cee30b61fe563aaee07bc1afe28f817ad9f1e08
nutme <egor.riter12@gmail.com>
2023-06-26 19:40:01 +0200

    added new model QuestComplite

--- quest/admin.py
+++ quest/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Quest
+from .models import Quest, QuestComplite
 # Register your models here.
 admin.site.register(Quest)
+admin.site.register(QuestComplite)
--- quest/models.py
+++ quest/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+User = get_user_model()
+
 class Quest(models.Model):
     title = models.CharField(max_length=50)
     body = models.TextField(max_length=500)
@@ -9,3 +12,9 @@ class Quest(models.Model):
 
     def __str__(self) -> str:
         return self.title
+    
+
+class QuestComplite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quest = models.ForeignKey(Quest, on_delete=models.CASCADE)
+    answer = models.SlugField(max_length=50)
