c88abb0e8ae31983fd0c6d8dc3a7e8b41752b552
nutme <egor.riter12@gmail.com>
2023-06-27 16:11:41 +0200

    added signals

--- core/models.py
+++ core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 User = get_user_model()
 class UserProfile(models.Model):
